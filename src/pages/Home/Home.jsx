2b59f7474ab8c3a58aacb744acc429c07c1863f7
Frank Rauen <home@Franks-MacBook-Air.local>
2020-03-04 21:33:58 -0700

    adds drink model

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,9 +22,19 @@ class Home extends Component {
         <div>
             <button onClick={() => this.handleGetDrinks('margarita')}>Marg</button>
             <button onClick={() => this.handleGetDrinks('bloody mary')}>Bloody Mary</button>
-            {this.state.drinks.map(({idDrink, strDrink}) => (
-                <div key={idDrink}>
+            {this.state.drinks.map(({idDrink, strDrink, strInstructions, 
+            strIngredient1, strIngredient2, strIngredient3, strIngredient4, strMeasure1, strMeasure2, strMeasure3, strMeasure4}) => (
+                <div key={idDrink} className='drinks-container'>
                     {strDrink}
+                    {strInstructions}
+                    {strIngredient1}
+                    {strMeasure1}
+                    {strIngredient2}
+                    {strMeasure2}
+                    {strIngredient3}
+                    {strMeasure3}
+                    {strIngredient4}
+                    {strMeasure4}
                 </div>
             ))}
         </div>)
