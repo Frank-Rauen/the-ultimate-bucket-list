558ecadab5a4bffc22675cdebffe7d18eb525324
Frank Rauen <home@Franks-MacBook-Air.local>
2020-03-07 09:05:03 -0700

    removes database connection uri

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
-mongoose.connect(process.env.DATABASE_URL || 'mongodb+srv://frankrauen:fjr5361!@cluster0-db3aj.mongodb.net/bartenders-best-friend?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASE_URL, {
         useNewUrlParser: true, 
         useCreateIndex: true,
         useUnifiedTopology: true 
